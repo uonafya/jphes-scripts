e433eced7343989c7f03d553a1cfd98dd7fb9061
Dennis Banga <bangadennis@users.noreply.github.com>
2017-05-04 01:24:32 +0300

    Create attribution.py

---
+++ attribution.py
@@ -0,0 +1,71 @@
+#Script for JPHES Attribution
+import psycopg2
+import pandas as pd
+import numpy as np
+
+nutrition_dataelementgroupid=2046015
+mch_dataelementgroupid=2046012
+monthly_periodtypeid=3
+nutrition_filter='Nutrition'
+mch_filter='MCH'
+
+count_executions=[]
+
+
+conn_str = "host={} dbname={} port={} user={} password={}".format("localhost", "port", "database", "user", "password")
+conn=None
+try:
+    conn = psycopg2.connect(conn_str)
+   
+    
+    ipsl_members=pd.read_sql("select * from jphes_ipsl", con=conn)
+    
+    #select data elements for a specific Program
+    dataelements=pd.read_sql("select dataelementid from dataelementgroupmembers \
+                             where dataelementgroupid={}".format(mch_dataelementgroupid), con=conn)
+    
+    #filter orgunits for a specific program
+    ipsl_orgunits=ipsl_members[ipsl_members.programs.str.contains(mch_filter,case=False)]
+    
+    #filter by quarter
+    ipsl_orgunits=ipsl_orgunits[ipsl_orgunits.period.str.contains("Q1", case=False)]
+    
+    #period
+    periods=pd.read_sql("select periodid,startdate, enddate from period \
+                        where startdate>='2016-10-1' and enddate<='2016-12-31' \
+                        and periodtypeid={}".format(monthly_periodtypeid), con=conn)
+    
+    
+    cursor = conn.cursor()
+
+    print("Started attribution process...")
+    for dataelementid in dataelements.dataelementid:
+        for period in periods.periodid:
+            for index, orgunit in ipsl_orgunits.iterrows():
+                    
+                cursor.execute("UPDATE datavalue set attributeoptioncomboid = %s WHERE sourceid = %s "
+                "AND dataelementid = %s AND periodid = %s" % (orgunit['mechanism_attributionid'], 
+                                              orgunit['orgunitid'], dataelementid, period))
+                
+                
+                count_executions.append(cursor.rowcount)
+                
+                conn.commit()
+    
+    cursor.close()
+    
+    print("---------------------------------------")
+    print("Successful Updates-{}".format(np.sum(count_executions)))
+    print("Total-{}".format(np.size(count_executions)))
+    print("---------------------------------------")
+
+    print("Attribution Complete")
+            
+                               
+except Exception as ex:
+                               
+    print("Error Updating-{}".format(ex))
+                               
+finally:
+        if conn is not None:
+            conn.close()
